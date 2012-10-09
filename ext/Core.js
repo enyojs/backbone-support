1179fde204dbf558cc47d0ada538bd39fda953a4
Cole Davis <cole.davis@palm.com>
2012-10-09 12:04:20 -0700

    add generic handler method for controllers

--- ext/Core.js
+++ ext/Core.js
@@ -7,4 +7,19 @@
     } else console.warn("No application found");
   };
   
+  // there is a concept for the ApplicationController, perhaps another
+  // type of controller altogether, that it has a "handle"
+  // capability that will accept an "event" or "trigger" and
+  // asynchronously execute the handler (if it has one) if
+  // the property matches the trigger/event as a general
+  // form of delegation
+  enyo._handle = function (trigger) {
+    if (trigger in this) {
+      enyo.asyncMethod(this, function (args) {
+        if (this.beforeHandler) this.beforeHandler(trigger);
+        this[trigger].apply(this, args);
+      }, arguments);
+    }
+  };
+  
 }());
