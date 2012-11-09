30cf753256975ebd8c66eb266d612ecc1deba7c5
Cole Davis <cole.davis@palm.com>
2012-11-09 12:07:18 -0800

    cleanup, document

--- ext/Core.js
+++ ext/Core.js
@@ -1,14 +1,25 @@
 (function () {
   
+  //*@public
+  /**
+    _MVC_ provides a pattern such that there is a singleton instance of
+    an _application_ and that this _application_ does not actually execute
+    until the _start_ method is fired. Methods can be registered in the
+    startup queue via the _enyo.run_ method. Calling _enyo.start_ will
+    ensure that this queue is flushed at the appropriate time and that the
+    registered application will begin its execution.
+  */
   enyo.start = function () {
     // TODO: this needs to be revisited but is a placeholder for now
     if (enyo._app) {
       enyo._app.start();
       enyo.isStarted = true;
       enyo.flushStartupQueue();
-    } else console.warn("No application found");
+    } else enyo.warn("No application found");
   };
   
+  //*@protected
+  // @TODO: REVISIT
   // there is a concept for the ApplicationController, perhaps another
   // type of controller altogether, that it has a "handle"
   // capability that will accept an "event" or "trigger" and
@@ -25,8 +36,16 @@
     } else return false;
   };
   
+  //*@protected
   enyo._queue = [];
   
+  //*@public
+  /**
+    Execute the method (with the optional context) when the framework
+    has received the _enyo.start_ command. If the framework has already
+    started, the method will be exected immediately, otherwise it will
+    be added to a queue and flushed at the appropriate time.
+  */
   enyo.run = function (fn, context) {
     var q = enyo._queue || [], args = enyo.toArray(arguments).slice(2);
     if (!fn) return false;
@@ -44,6 +63,7 @@
     }
   };
   
+  //*@protected
   enyo.flushStartupQueue = function () {
     var q = enyo._queue, fn;
     if (!q) return true;
@@ -53,11 +73,13 @@
     }
   };
   
-  // TODO: WTF!??!
+  //*@protected
+  // preserve the original constructors
   enyo.Backbone = enyo.Backbone? enyo.Backbone: {};
   enyo.Backbone.Model = Backbone.Model;
   enyo.Backbone.Collection = Backbone.Collection;
-  // now attempt to map models to...
+  // replace the constructors Backbone will be using
+  // with our enyo.Extensions
   Backbone.Model = enyo.Model;
   Backbone.Collection = enyo.Collection;
   
