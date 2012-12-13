8d577251754d19a39a360e210c9766bd929b63d4
Cole Davis <cole.davis@palm.com>
2012-12-12 16:16:11 -0800

    rewrite mixin base
    Enyo-DCO-1.1-Signed-off-by: Cole Davis <cole.davis@palm.com>

--- ext/CollectionRowMixin.js
+++ ext/CollectionRowMixin.js
@@ -1,82 +1,127 @@
+
 //*@public
 /**
-   A special _enyo.Mixin_ used by _enyo.CollectionRepeater_ and
-  _enyo.CollectionList_ to apply certain properties to row controls.
+    _enyo.CollectionRowMixin_ is an _enyo.Mixin_ kind used by
+    _enyo.CollectionList_ and _enyo.CollectionRepeater_ that enables
+    auto-bindings on child views that have a _bindProperty_ and
+    _bindTarget_ properties.
+    
+    Note that this mixin is responsible for applying the
+    _enyo.CollectionRowControllerMixin_ to the row's controller.
 */
 enyo.Mixin({
-  //*@protected
-  name: "enyo.CollectionRowMixin",
-  //*@protected
-  initAutoBindings: function () {
-    var c = this.controller, ctrs = this.get("bindableControls");
-    if (!c) return;
-    enyo.forEach(ctrs, function (ch) {
-      var p = ch.bindProperty, t = this.getBindTargetFor(ch);
-      this.autoBinding({source: c, from: p, target: ch, to: t});
-    }, this);
-    this.autoBinding({source: c, from: "selected", target: this, to: "selected"});
-  },
-  //*@protected
-  selectedChanged: function () {
-      this.addRemoveClass("selected", this.selected);
-  },
-  //*@protected
-  initMixin: function () {
-    // first make sure we have a controller
-    if (!this.controller) {
-      // this will fire the correct notifications to
-      // properly setup the controller, and our responder
-      // and we won't need to call initAutoBindings
-      this.set("controller", "enyo.ModelController");
-    } else this.initAutoBindings();
-  },
-  //*@protected
-  destroyMixin: function () {
-    this.clearAutoBindings();
-  },
-  //*@protected
-  getBindTargetFor: function (inControl) {
-    var c = inControl, r = c.get("bindTarget");
-    if (!r) r = ".content";
-    r = r[0] === "."? r: "." + r;
-    return r;
-  },
-  //*@protected
-  autoBinding: function () {
-    var b = this.binding.apply(this, arguments);
-    b.isAutoBinding = true;
-    return b;
-  },
-  //*@protected
-  autoBindings: enyo.Computed(function () {
-    return enyo.filter(this._bindings, function (b) {return b.isAutoBinding === true});
-  }),
-  //*@protected
-  controllerChanged: function () {
-    this.inherited(arguments);
-    this.clearAutoBindings();
-    this.initAutoBindings();
-  },
-  //*@protected
-  clearAutoBindings: function () {
-    var a = this.get("autoBindings"), b;
-    while (a.length) {
-      b = a.shift();
-      b.destroy();
+    //*@protected
+    name: "enyo.CollectionRowMixin",
+    // if we don't have a controller we create the generic model controller
+    // for the view otherwise we initialize the auto bindings since setting
+    // the controller will cause the controllerChanged observer to fire and
+    // that automatically fires the initAutoBindings method
+    initMixin: function () {
+        if (!this.controller) {
+            this.set("controller", "enyo.ModelController");
+        } else this.initAutoBindings();
+    },
+    // clean up all the auto bindings when the view is destroyed
+    destroyMixin: function () {
+        this.clearAutoBindings();
+    },
+    // here we create what would otherwise be normal bindings but
+    // give them a special property so we can select them via the
+    // computed property (a filter on the _bindings property of the
+    // view), but the bindings we create are using the controller of
+    // the view as the source, the view as the target, the
+    // bindProperty as the property on the controller to retrieve
+    // (from) and the bindTarget (or content in its absence) (to)
+    initAutoBindings: function () {
+        var controller = this.controller;
+        var controls = this.get("bindableControls");
+        if (!controller) return;
+        enyo.forEach(controls, function (child) {
+            var prop = child.bindProperty;
+            var to = this.getBindTargetFor(child);
+            this.autoBinding({
+                source: controller,
+                from: prop,
+                target: child,
+                to: to
+            });
+        }, this);
+        this.autoBinding({
+            source: controller,
+            from: "selected",
+            target: this,
+            to: "selected"
+        });
+    },
+    // when the model's selected property changes we attempt
+    // to update accordingly - the default is to place a
+    // "selected" class on the row and if the developer
+    // wishes they can supply the appropriate CSS to display
+    // the row differently
+    selectedChanged: function () {
+        this.addRemoveClass("selected", this.selected);
+    },
+    // figure out if a bindTarget was put on the child and
+    // if not get set it to content, if it was set make sure
+    // it has the correct format
+    getBindTargetFor: function (child) {
+        var to = child.get("bindTarget");
+        if (!to) to = ".content";
+        to = to[0] === "."? to: "." + to;
+        return to;
+    },
+    // this is the function that essentially wraps the default
+    // binding method on object, add the isAutoBinding boolean
+    autoBinding: function () {
+        var binding = this.binding.apply(this, arguments);
+        binding.isAutoBinding = true;
+        return binding;
+    },
+    // computed property that will return the array of auto bindings
+    // for this view
+    autoBindings: enyo.Computed(function () {
+        return enyo.filter(this._bindings, function (binding) {
+            return binding.isAutoBinding === true;
+        });
+    }),
+    // auto fire this changed event when the controller is updated
+    controllerChanged: function () {
+        this.inherited(arguments);
+        if (this.controller) {
+            this.controller.extend(enyo.CollectionRowControllerMixin);
+            this.clearAutoBindings();
+            this.initAutoBindings();
+        }
+    },
+    // make sure to remove all of the auto bindings, this needed
+    // to be separate from the normal clearBindings of the view
+    // because this can happen even when the view is still active
+    clearAutoBindings: function () {
+        var autos = this.get("autoBindings");
+        var binding;
+        while(autos.length) {
+            binding = autos.shift();
+            binding.destroy();
+        }
+    },
+    // computed property for bindable controls in an array
+    bindableControls: enyo.Computed(function () {
+        var controls = enyo.clone(this.controls);
+        controls.unshift(this);
+        return this.findBindableControls(controls);
+    }),
+    // figure out what the bindable controls are including this
+    // view and all of its children
+    // recursive decent search for bindable controls
+    findBindableControls: function (controls) {
+        var ret = [];
+        var fn;
+        if (!controls || controls.length === 0) return controls;
+        fn = enyo.bind(this, this.findBindableControls);
+        enyo.forEach(controls, function (control) {
+            ret = ret.concat(fn(control.controls || []));
+            if (control.bindProperty) ret.push(control);  
+        });
+        return ret;
     }
-  },
-  bindableControls: enyo.Computed(function () {
-    var ctrs = enyo.clone(this.controls);
-    ctrs.unshift(this);
-    return this.findBindableControls(ctrs);
-  }),
-  findBindableControls: function (controls) {
-    if (!controls || controls.length === 0) return controls;
-    var ret = [], fn = enyo.bind(this, this.findBindableControls);
-    enyo.forEach(controls, function (control) {
-      ret = ret.concat(fn(control.controls || []));
-      if (control.bindProperty) ret.push(control);
-    });
-    return ret;
-  }
-});
+})
