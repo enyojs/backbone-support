7ccf8a4dbe9cf475b331ce8dbc6caf7c110980b3
W. Cole Davis <wcole.davis@gmail.com>
2013-01-06 15:58:08 -0800

    update build scripts to include additional foss libs
    Enyo-DCO-1.1-Signed-off-by: Cole Davis <cole.davis@palm.com>

--- deploy.bat
+++ deploy.bat
@@ -26,6 +26,9 @@ GOTO :EOF
 REM copy assets
 XCOPY %SOURCE%foss\backbone\backbone-min.js %TARGET%\foss\backbone\backbone.js /Q /E
 XCOPY %SOURCE%foss\underscore\underscore-min.js %TARGET%\foss\underscore\underscore.js /Q /E
+XCOPY %SOURCE%foss\backbone-relational\backbone-relational.js %TARGET%\foss\backbone-relational\backbone-relational.js /Q /E
+XCOPY %SOURCE%foss\jquery\jquery-1.8.3-min.js %TARGET%\foss\jquery\jquery-1.8.3-min.js /Q /E
+
 FOR %%DIR IN ("controllers" "ext" "models" "views") DO (
   XCOPY %SOURCE%%%DIR\*.* %TARGET%\%%DIR\ /Q /E
 )
--- deploy.sh
+++ deploy.sh
@@ -15,8 +15,12 @@ EOF
 else
   mkdir -p $TARGET/foss/backbone
   mkdir -p $TARGET/foss/underscore
+  mkdir -p $TARGET/foss/backbone-relational
+  mkdir -p $TARGET/foss/jquery
   cp $SOURCE/foss/backbone/backbone-min.js $TARGET/foss/backbone/backbone.js
   cp $SOURCE/foss/underscore/underscore-min.js $TARGET/foss/underscore/underscore.js
+  cp $SOURCE/foss/backbone-relational/backbone-relational.js $TARGET/foss/backbone-relational/backbone-relational.js
+  cp $SOURCE/foss/jquery/jquery-1.8.3-min.js $TARGET/foss/jquery/jquery-1.8.3-min.js
   for dir in "controllers" "ext" "models" "views"
   do
     cp -r $SOURCE/$dir $TARGET/
