ad2b0c0c8844783ee09e6764df2c494ff8458832
Cole Davis <cole.davis@palm.com>
2013-01-25 11:31:58 -0800

    simplify...
    Enyo-DCO-1.1-Signed-off-by: Cole Davis <cole.davis@palm.com>

--- views/CollectionRepeater.js
+++ views/CollectionRepeater.js
@@ -18,9 +18,6 @@ enyo.kind({
     
     //*@public
     model: null,
-    
-    //*@public
-    mixins: ["enyo.CommonCollectionViewMixin"],
 
     // ...........................
     // PROTECTED PROPERTIES
@@ -37,24 +34,6 @@ enyo.kind({
     // ...........................
     // PROTECTED METHODS
     
-    
-    //*@protected
-    didChange: function (sender, event) {
-        this.log(event);
-        var values = event.values;
-        var indices = enyo.keys(values);
-        var idx;
-        var len = indices.length;
-        var children = this.children;
-        var child;
-        var pos = 0;
-        for (; pos < len; ++pos) {
-            idx = indices[pos];
-            child = children[idx];
-            if (!child) continue;
-            child.controller.set("data", values[idx]);
-        }
-    }
     // ...........................
     // OBSERVERS
     
